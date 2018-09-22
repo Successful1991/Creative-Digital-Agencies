94a4c20ae42819c10c9d63065069d3b0460a498c
Tsys Artem <19successful91@gmail.com>
2018-09-22 21:08:29 +0300

    head + abount

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+var cssmin = require('gulp-cssmin');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+
+
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+
+//--server--//
+gulp.task('server', function() {
+	gulp.src('')
+		.pipe(server({
+			livereload: true,
+			open: true
+		}));
+});
+
+//--compiler--//
+gulp.task('sass', function () {
+
+	return gulp.src('app/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(prefix({
+			browsers:['last 6 versions']
+		}))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('app/sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['server','watch']);
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}});
+});
+
+// gulp.task('jsx', function () {
+//   return gulp.src('app/js/index.js')
+//     .pipe(sourcemaps.init())
+//     .pipe(babel({
+//       presets: ['@babel/env']
+//     }))
+//     //.pipe(concat('all.js'))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('build/js'));
+// });
+
+gulp.task('min-js', function (cb) {
+  pump([
+      gulp.src('app/js/*.js'),
+      babel({
+        presets: ['@babel/env']
+      }),
+      uglify(),
+      rename({suffix: '.min'}),
+      gulp.dest('build/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('image-min', () =>
+  gulp.src(['app/img/*','app/img/**/*'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'))
+);
+
+gulp.task('min-css', function () {
+  gulp.src('app/css/*.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('transfer-font', function () {
+  gulp.src('app/fonts/*.*')
+    .pipe(gulp.dest('build/fonts'));
+});
+
+
+gulp.task('clean', function () {
+  gulp.src('build', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('compile', ['clean','min-js','image-min','min-css','transfer-font','transfer-min']);
